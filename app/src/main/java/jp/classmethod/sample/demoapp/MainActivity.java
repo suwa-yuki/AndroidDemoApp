8909ab2eaeba6c1f651d54e3582f59d08ca86d71
suwa.yuki <suwa.yuki@classmethod.jp>
2016-07-12 12:22:16 +0900

    [modify] Change build tools version

--- app/src/main/java/jp/classmethod/sample/demoapp/MainActivity.java
+++ app/src/main/java/jp/classmethod/sample/demoapp/MainActivity.java
@@ -1,8 +1,10 @@
 package jp.classmethod.sample.demoapp;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Log.i("DemoApp", "ModelName : " + Build.MODEL);
     }
 
     @Override
